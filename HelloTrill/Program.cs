34266b99cc245e62c66db0c33abe48278990b87d
提莫 <temo@timodeMacBook-Pro.local>
2021-05-03 21:59:49 -0400

    fix some queries

--- HelloTrill/Program.cs
+++ HelloTrill/Program.cs
@@ -12,77 +12,90 @@ namespace Test
             /**
              * Generating synthetic data
              */
-            var SIZE = 1000;                                    // Size of the data set
-            var listA = new List<int>();                        // A list for storing the data points
-            var listB = new List<int>();                        // Another list for storing the data points
+            var SIZE = 1000; // Size of the data set
+            var listA = new List<int>(); // A list for storing the data points
+            var listB = new List<int>(); // Another list for storing the data points
             for (int i = 0; i < SIZE; i++)
             {
-                listA.Add(i);                                    // Populate listA with dummy data
-                listB.Add(i);                                    // Populate listB with dummy data
+                listA.Add(i); // Populate listA with dummy data
+                listB.Add(i); // Populate listB with dummy data
             }
-            
+
             /**
              * Creating lists created above to Trill streams
              */
-            var streamA = listA                                 // Creating first stream from listA 
-                    .ToObservable()                             // Convert the data list to an Observable first 
-                    .ToTemporalStreamable(e => e, e => e + 1)   // Then convert to Trill temporal stream;
-                ;                                               // nth event in the stream has an integer payload 'n'
-                                                                // and an interval of [n, n+1)
-            
-            var streamB = listB                                 // Creating streamB (not using yet) similar to streamA.
+            var streamA = listA // Creating first stream from listA 
+                    .ToObservable() // Convert the data list to an Observable first 
+                    .ToTemporalStreamable(e => e, e => e + 1) // Then convert to Trill temporal stream;
+                ; // nth event in the stream has an integer payload 'n'
+            // and an interval of [n, n+1)
+
+            var streamB = listB // Creating streamB (not using yet) similar to streamA.
                     .ToObservable()
                     .ToTemporalStreamable(e => e, e => e + 100)
                 ;
-            
-            /**
-             * Define transformations on the stream(s) 
-             */
-            var level1_result = streamA
-                    .Select(e => 3 * (e + 1))                         // Set transformations on the stream.
-                    .Where(e => e % 2 == 1)
 
-                ;                                               // In this case, Adding 1 to each payload using Select
             
-            /**
-             * Print out the result
-             
-            level1_result
-                .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
-                .Where(e => e.IsData)                           // Only pick data events from the stream
-                .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
-                ;
-            */
+            
+            // 1.1
+            var level1_1 = streamA
+                .Select(e => e + 1);
 
-            /*
-            var level2_result = streamA
+            var level1_2 = streamA
+                .Select(e => 3 * e);
+
+            var level1_3 = streamA
+                .Select(e => 3 * (e + 1)) 
+                .Where(e => e % 2 == 1);
+
+            var level2_1 = streamA.ShiftEventLifetime(1)
                 .TumblingWindowLifetime(10)
+                .ShiftEventLifetime(-10)
                 .Sum(e => e);
-            level2_result
+
+            
+            streamB
                 .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
                 .Where(e => e.IsData)                           // Only pick data events from the stream
                 .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
                 ;
+            
+            /*
+            
+            var level2_2 = streamA.
+                Join(streamB, (l, r) => l + r);
+            
+            level2_2
+                .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
+                .Where(e => e.IsData)                           // Only pick data events from the stream
+                .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
+                ;
+            
             */
 
+
             /*
-            var joinA_B = streamA.Join(streamB, e => e, e => e, (l, r) => l + r);
-            joinA_B
-                .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
-                .Where(e => e.IsData)                           // Only pick data events from the stream
-                .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
-                ;
-            */
-            
-            
-            
             var joinA_B_pair = streamA.Join(streamB, e => e, e => e, (l, r) => new {l, r});
             joinA_B_pair
                 .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
                 .Where(e => e.IsData)                           // Only pick data events from the stream
                 .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
                 ;
-            
+            */
+
+            /*
+             var window_sum_B = streamB
+                .TumblingWindowLifetime(10)
+                .Sum(e => e)
+                .Join(streamA, (l, r) => new{ l, r });
+            streamA
+                .ToStreamEventObservable()                      // Convert back to Observable (of StreamEvents)
+                .Where(e => e.IsData)                           // Only pick data events from the stream
+                .ForEach(e => { Console.WriteLine(e); })        // Print the events to the console
+                ;
+            */
+            // streamA.Multicast(s => s.TumblingWindowLifetime(10).Sum(e => e));
+
         }
     }
 }
